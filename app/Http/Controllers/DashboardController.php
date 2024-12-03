201bac9ff949d04294ac17e379216c9a23d629da
bhavanagattha <bhavanagatthawongso@gmail.com>
2024-12-03 13:52:54 +0700

    bugfix #11

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+use App\Models\TransaksiDetail;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count(); 
+        $total_items_sold = TransaksiDetail::sum('jumlah'); 
+        $total_revenue = TransaksiDetail::sum('harga_total'); 
+
+        return view('dashboard', [
+            'transaksi_count' => $transaksi_count,
+            'total_items_sold' => $total_items_sold,
+            'total_revenue' => $total_revenue,
+        ]);
     }
 }
